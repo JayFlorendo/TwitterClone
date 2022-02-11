b4a1eead13ef35c1de5715604846d66a2f452b39
JayFlorendo <64466300+JayFlorendo@users.noreply.github.com>
2022-02-12 03:42:37 +0800

    SQL Table

---
+++ dbo.Tweets.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Tweets] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [userId]      INT            NULL,
+    [username]    VARCHAR (MAX)  NULL,
+    [tweet]       NVARCHAR (MAX) NULL,
+    [dateTweeted] DATETIME       NULL,
+    [profileName] NVARCHAR (MAX) NULL,
+    [picture]     NVARCHAR (MAX) NULL,
+    [heart]       INT            NULL DEFAULT 0,
+    [retweet]     INT            NULL DEFAULT 0,
+    [reply]       INT            NULL DEFAULT 0,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ dbo.Users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]       INT            NOT NULL,
+    [username] NVARCHAR (MAX) NULL,
+    [password] NVARCHAR (MAX) NOT NULL,
+    [name] NVARCHAR(MAX) NULL, 
+    [picture] NVARCHAR(MAX) NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
